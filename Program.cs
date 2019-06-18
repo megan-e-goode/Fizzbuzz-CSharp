daf3d1663845fd10f26317d4f9d59c8b34e74e5a
Megan Goode <megan.e.goode@hotmail.com>
2019-06-18 11:11:52 +0100

    Fizzbuzz code in CSharp

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fizzbuzz_cSharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 100; i++) {
+                Console.WriteLine(Fizzbuzz(i));
+            }
+            
+        }
+
+        public static string Fizzbuzz(int number) 
+        {
+            if(number == 0) {
+                return number.ToString();
+            } else if (number % 3 == 0 && number % 5 == 0) {
+                return "Fizzbuzz";
+            } else if(number % 3 == 0) {
+                return "Fizz";
+            } else if(number % 5 == 0) {
+                return "Buzz";
+            } else {
+                return number.ToString();
+            }
+        }
+    }
+}
